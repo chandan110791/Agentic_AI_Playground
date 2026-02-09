d3f85ba8b0a703c8fafd5d4386887788c16e6efa
chandan110791 <chandan110791@github.com>
2026-02-09 19:40:35 +0400

    changes

--- P3_CallBacks/CallBacksAgent/agentcallbacks/agent.py
+++ P3_CallBacks/CallBacksAgent/agentcallbacks/agent.py
@@ -54,5 +54,5 @@ def after_agent_callback(text:str,callback_context:CallbackContext)->Optional[ty
 
 
 
-agent_callback_ex = LlmAgent(name="agentcallbacks",model="",instruction="You are a LLM.We are here to demonstrate agent callbacks. You have below tool available to calulcate the share the counter and ",
+root_agent = LlmAgent(name="agentcallbacks",model="",instruction="You are a LLM.We are here to demonstrate agent callbacks. You have below tool available to calulcate the share the counter and ",
                                 description="agent to demonstarte agent callback",before_agent_callback=[before_agent_callback],after_agent_callback=[after_agent_callback])
